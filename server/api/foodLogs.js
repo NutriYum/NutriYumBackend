767ca28ae1958e2b38a30f81069256d4df09370a
joeydeeds <joey.deeds@gmail.com>
2018-05-13 22:52:32 -0500

    adding to food log api route

--- server/api/foodLogs.js
+++ server/api/foodLogs.js
@@ -1,28 +1,26 @@
 const router = require('express').Router()
-const {FoodLogs} = require('../db/models')
+const { FoodLogs } = require('../db/models')
 
 router.param('foodLogsId', async (req, res, next, foodId) => {
   try {
-    const food = await FoodLogs.findById(foodId);
-    req.food = food;
-    next();
-  }
-  catch (err) {
-    next(err);
-  }
-})
-
-router.get('/',  async (req, res, next) => {
-  try {
-    const allFoodLogs = await FoodLogs.findAll()
-    res.json(allFoodLogs)
-  }
-  catch (err){
+    const food = await FoodLogs.findById(foodId)
+    req.food = food
+    next()
+  } catch (err) {
     next(err)
   }
 })
 
-router.get('/:id',  async (req, res, next) => {
+router.get('/', async (req, res, next) => {
+  try {
+    const allFoodLogs = await FoodLogs.findAll()
+    res.json(allFoodLogs)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
   try {
     const id = req.params.id
     const userFoodLogs = await FoodLogs.findAll({
@@ -31,21 +29,41 @@ router.get('/:id',  async (req, res, next) => {
       }
     })
     res.json(userFoodLogs)
-  }
-  catch (err){
+  } catch (err) {
     next(err)
   }
 })
 
-router.post('/',  async (req, res, next) => {
+router.post('/', async (req, res, next) => {
+  // name: 'bagel',
+  // sugar: 8.85,
+  // calories: 277.2,
+  // totalFat: 1.39,
+  // sodium: 443.1,
+  // protein: 11.09,
+  // carbs: 55,
+  // servingUnit: 'medium bagel',
+  // quantity: 1
   try {
-    const newFood = await FoodLogs.create(req.body)
+    const newFood = await req.body.forEach(element => {
+      return FoodLogs.create({
+        name: element.name,
+        sugar: element.sugar,
+        calories: element.calories,
+        totalFat: element.totalFat,
+        sodium: element.sodium,
+        protein: element.protein,
+        carbs: element.carbs,
+        servingUnit: element.servingUnit,
+        quantity: element.quantity,
+        userId: req.user.id
+      })
+    })
+
     res.json(newFood)
-  }
-  catch (err){
+  } catch (err) {
     next(err)
   }
 })
-
 
 module.exports = router
