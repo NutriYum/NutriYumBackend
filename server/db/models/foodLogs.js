da87eeabd186fc440ebdea501addde1589c4a645
Dan Feeney <dfeeney2456@gmail.com>
2018-05-14 12:29:38 -0500

    models and api ready

--- server/db/models/foodLogs.js
+++ server/db/models/foodLogs.js
@@ -27,15 +27,57 @@ const FoodLogs = db.define('foodLogs', {
     },
     userId: {
         type: Sequelize.INTEGER,
-    }, 
+    },
     servingUnit: {
         type: Sequelize.STRING
-    }, 
+    },
     quantity: {
         type: Sequelize.INTEGER
-    }, 
-    
+    },
+
 })
 
+function lessDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() - days);
+    return result;
+  }
+
+  const Op = Sequelize.Op
+
+FoodLogs.oneDay = function (userId){
+    return this.findAll({
+        where: {
+            $and: [
+                { userId: userId },
+                Sequelize.where(
+                   Sequelize.fn('DATE', Sequelize.col('createdAt')),
+                   Sequelize.literal('CURRENT_DATE')
+                )
+            ]
+         }
+    })
+}
+
+FoodLogs.oneWeek = function (userId){
+    let endDate = lessDays(Date.now(), 7)
+    return this.findAll({
+        where: {
+            userId: userId,
+            createdAt: {[Op.gte]: endDate}
+         }
+    })
+}
+
+FoodLogs.oneMonth = function (userId){
+    let endDate = lessDays(Date.now(), 30)
+    return this.findAll({
+        where: {
+            userId: userId,
+            createdAt: {[Op.gte]: endDate}
+         }
+    })
+
+}
 
 module.exports = FoodLogs
